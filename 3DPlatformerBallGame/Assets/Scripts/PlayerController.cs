6d5004487156e30a18781841d74b8c2a8bf92577
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-04-05 17:03:16 +0300

    Update PlayerController.cs

--- 3DPlatformerBallGame/Assets/Scripts/PlayerController.cs
+++ 3DPlatformerBallGame/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float pushForce;
+    public float speed;
+    private float movement;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
     void FixedUpdate()
     {
         rb.AddForce(0,0,pushForce * Time.fixedDeltaTime);
+        rb.velocity = new Vector3(movement * speed ,rb.velocity.y,rb.velocity.z);
     }
 }
